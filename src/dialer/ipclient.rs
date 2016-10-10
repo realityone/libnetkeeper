50bacb0c8508971d0bc61f038f8b6ac47a629e2b
realityone <realityone@me.com>
2016-10-11 01:02:52 +0800

    restruct MACOpenDialer as MACOpenPacket

--- src/dialer/ipclient.rs
+++ src/dialer/ipclient.rs
@@ -1,7 +1,6 @@
 use std::net::Ipv4Addr;
 use std::num::Wrapping;
 
-use dialer::Dialer;
 use utils::integer_to_bytes;
 
 // copy from https://github.com/xuzhipengnt/ipclient_gxnu
@@ -11,12 +10,15 @@ const MAC_ADDRESS_LEN: usize = 18;
 #[derive(Debug)]
 pub enum MACOpenErr {
     UsernameTooLong,
-    MACAddressTooLong,
+    MACAddressError,
 }
 
 #[derive(Debug)]
-pub struct MACOpenDialer {
-    hash_key: u32,
+pub struct MACOpenPacket {
+    username: String,
+    ipaddress: Ipv4Addr,
+    mac_address: String,
+    isp: ISPCode,
 }
 
 #[derive(Debug)]
@@ -25,7 +27,7 @@ pub enum Configuration {
     GXNU,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum ISPCode {
     CChinaUnicom = 1 << 8,
     CChinaTelecom = 2 << 8,
@@ -40,48 +42,54 @@ impl Configuration {
     }
 }
 
-impl MACOpenDialer {
-    pub fn new(hash_key: u32) -> Self {
-        MACOpenDialer { hash_key: hash_key }
+impl MACOpenPacket {
+    pub fn new(username: &str, ipaddress: Ipv4Addr, mac_address: &str, isp: ISPCode) -> Self {
+        MACOpenPacket {
+            username: username.to_string(),
+            ipaddress: ipaddress,
+            mac_address: mac_address.to_string(),
+            isp: isp,
+        }
     }
 
-    pub fn calculate_packet(&self,
-                            username: &str,
-                            ipaddress: Ipv4Addr,
-                            mac_address: &str,
-                            isp: ISPCode)
-                            -> Result<Box<Vec<u8>>, MACOpenErr> {
-        if username.len() > USERNAME_MAX_LEN - 1 {
-            return Err(MACOpenErr::UsernameTooLong);
-        }
-        if mac_address.len() != MAC_ADDRESS_LEN - 1 {
-            return Err(MACOpenErr::MACAddressTooLong);
-        }
+    pub fn as_bytes(&self, hash_key: u32) -> Result<Box<Vec<u8>>, MACOpenErr> {
+        try!(self.validate());
 
         let mut macopen_packet: Box<Vec<u8>> = Box::new(Vec::with_capacity(60));
         {
             let mut username_bytes = [0; USERNAME_MAX_LEN];
             let mut mac_address_bytes = [0; MAC_ADDRESS_LEN];
-            username_bytes[..username.len()].clone_from_slice(username.as_bytes());
-            mac_address_bytes[..mac_address.len()].clone_from_slice(mac_address.as_bytes());
+            username_bytes[..self.username.len()].clone_from_slice(self.username.as_bytes());
+            mac_address_bytes[..self.mac_address.len()]
+                .clone_from_slice(self.mac_address.as_bytes());
 
-            let isp_be = (isp as u32).to_be();
+            let isp_be = (self.isp.clone() as u32).to_be();
             let isp_bytes = integer_to_bytes(&isp_be);
 
             macopen_packet.extend(&username_bytes);
-            macopen_packet.extend(&ipaddress.octets());
+            macopen_packet.extend(&self.ipaddress.octets());
             macopen_packet.extend(&mac_address_bytes);
             macopen_packet.extend(isp_bytes);
 
-            let hash_bytes = self.hash_bytes(&macopen_packet);
+            let hash_bytes = Self::hash_bytes(&macopen_packet, hash_key);
             macopen_packet.extend(&hash_bytes);
         }
 
         Ok(macopen_packet)
     }
 
-    pub fn hash_bytes(&self, bytes: &[u8]) -> [u8; 4] {
-        let mut hash = Wrapping(self.hash_key as i32);
+    fn validate(&self) -> Result<(), MACOpenErr> {
+        if self.username.len() > USERNAME_MAX_LEN - 1 {
+            return Err(MACOpenErr::UsernameTooLong);
+        }
+        if self.mac_address.len() != MAC_ADDRESS_LEN - 1 {
+            return Err(MACOpenErr::MACAddressError);
+        }
+        Ok(())
+    }
+
+    fn hash_bytes(bytes: &[u8], hash_key: u32) -> [u8; 4] {
+        let mut hash = Wrapping(hash_key as i32);
         for c in bytes.iter() {
             hash ^= (hash << 5) + (hash >> 2) + Wrapping(*c as i32);
         }
@@ -93,25 +101,16 @@ impl MACOpenDialer {
     }
 }
 
-impl Dialer for MACOpenDialer {
-    type C = Configuration;
-
-    fn load_from_config(config: Self::C) -> Self {
-        MACOpenDialer::new(config.hash_key())
-    }
-}
 
 #[test]
 fn test_mac_opener_hash_bytes() {
-    let dialer = MACOpenDialer::load_from_config(Configuration::GUET);
-
     let bytes1 = [1, 2, 3, 4, 5, 6, 7, 0];
-    let hash_bytes1 = dialer.hash_bytes(&bytes1);
+    let hash_bytes1 = MACOpenPacket::hash_bytes(&bytes1, Configuration::GUET.hash_key());
 
     let bytes2 = [97, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                   0, 0, 0, 0, 172, 16, 1, 1, 52, 48, 58, 54, 49, 58, 56, 54, 58, 56, 55, 58, 57,
                   70, 58, 70, 49, 0, 0, 0, 1, 0];
-    let hash_bytes2 = dialer.hash_bytes(&bytes2);
+    let hash_bytes2 = MACOpenPacket::hash_bytes(&bytes2, Configuration::GUET.hash_key());
 
     assert_eq!(hash_bytes1, [0x9c, 0x89, 0xf8, 0x3d]);
     assert_eq!(hash_bytes2, [255, 189, 40, 90]);
--- src/lib.rs
+++ src/lib.rs
@@ -59,21 +59,28 @@ mod tests {
     fn test_ipclient_macopener_packet() {
         use std::str::FromStr;
         use std::net::Ipv4Addr;
-        use dialer::ipclient::{MACOpenDialer, Configuration, ISPCode};
+        use dialer::ipclient::{MACOpenPacket, ISPCode, Configuration};
 
-        let dialer = MACOpenDialer::load_from_config(Configuration::GUET);
-        let packet = dialer.calculate_packet("a",
-                              Ipv4Addr::from_str("172.16.1.1").unwrap(),
-                              "40:61:86:87:9F:F1",
-                              ISPCode::CChinaUnicom)
-            .unwrap();
+        let packet = MACOpenPacket::new("a",
+                                        Ipv4Addr::from_str("172.16.1.1").unwrap(),
+                                        "40:61:86:87:9F:F1",
+                                        ISPCode::CChinaUnicom);
+        let packet_bytes = packet.as_bytes(Configuration::GUET.hash_key()).unwrap();
 
         let real_bytes: Vec<u8> = vec![97, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 172, 16, 1, 1, 52, 48, 58,
                                        54, 49, 58, 56, 54, 58, 56, 55, 58, 57, 70, 58, 70, 49, 0,
                                        0, 0, 1, 0, 255, 189, 40, 90];
 
-        assert_eq!(*packet, real_bytes);
+        assert_eq!(*packet_bytes, real_bytes);
+
+        let packet_err = MACOpenPacket::new("05802278989@HYXY.XY05802278989@HYXY.\
+                                             XY05802278989@HYXY.XY05802278989@HYXY.\
+                                             XY05802278989@HYXY.XY",
+                                            Ipv4Addr::from_str("172.16.1.1").unwrap(),
+                                            "40:61:86:87:9F:F1",
+                                            ISPCode::CChinaUnicom);
+        assert!(packet_err.as_bytes(Configuration::GUET.hash_key()).is_err());
     }
 
     #[test]
