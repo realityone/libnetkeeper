99f5e5dfe7a279592fcc1216da7295f4ccacc5b4
realityone <realityone@me.com>
2016-11-28 10:50:13 +0800

    reader macro to trait

--- src/heartbeater/reader.rs
+++ src/heartbeater/reader.rs
@@ -10,42 +10,35 @@ pub enum ReadBytesError {
     IOError(io::Error),
 }
 
-#[macro_export]
-macro_rules! read_bytes {
-    ($r:expr, $n:expr) => (
-        {
-            let result: Result<Vec<u8>, ReadBytesError>;
-            let mut bytes_container: Vec<u8> = vec![0; $n];
-            result = match $r.read(&mut bytes_container) {
-                Ok(length) => {
-                    if length != $n {
-                        Err(ReadBytesError::LengthMismatch($n, length))
-                    } else {
-                        Ok(bytes_container)
-                    }
-                },
-                Err(e) => {
-                    Err(ReadBytesError::IOError(e))
-                },
-            };
-            result 
+pub trait ReaderHelper: io::Read {
+    fn read_bytes(&mut self, required_length: usize) -> Result<Vec<u8>, ReadBytesError> {
+        let mut bytes_container: Vec<u8> = vec![0; required_length];
+        match self.read(&mut bytes_container) {
+            Ok(length) => {
+                if length != required_length {
+                    Err(ReadBytesError::LengthMismatch(required_length, length))
+                } else {
+                    Ok(bytes_container)
+                }
+            }
+            Err(e) => Err(ReadBytesError::IOError(e)),
         }
-    )
+    }
 }
+
+impl<R: io::Read> ReaderHelper for io::BufReader<R> {}
 
 #[test]
 fn test_read_bytes() {
-    use std::io::{BufReader, Read};
-
     let bytes: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let mut buffer = BufReader::new(&bytes as &[u8]);
+    let mut buffer = io::BufReader::new(&bytes as &[u8]);
 
-    let bytes03 = read_bytes!(&mut buffer, 3).unwrap();
+    let bytes03 = buffer.read_bytes(3).unwrap();
     assert_eq!(bytes03, vec![1, 2, 3]);
 
-    let bytes45 = read_bytes!(&mut buffer, 2).unwrap();
+    let bytes45 = buffer.read_bytes(2).unwrap();
     assert_eq!(bytes45, vec![4, 5]);
 
-    let bytes611 = read_bytes!(&mut buffer, 6);
+    let bytes611 = buffer.read_bytes(6);
     assert!(bytes611.is_err());
 }
