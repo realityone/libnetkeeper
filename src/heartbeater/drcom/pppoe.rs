3146f641bdbbe01a4e5b30af64a98f60447b8a36
realityone <realityone@me.com>
2017-01-28 23:00:46 +0800

    add none hasher in dr.com pppoe heartbeat

--- src/heartbeater/drcom/pppoe.rs
+++ src/heartbeater/drcom/pppoe.rs
@@ -1,4 +1,3 @@
-// use openssl::crypto::hash::{Hasher, Type};
 use byteorder::{NativeEndian, ByteOrder};
 use crypto::hash::{HasherBuilder, Hasher, HasherTypes};
 
@@ -33,9 +32,23 @@ trait CRCHasher {
     }
 }
 
+struct NoneHasher;
+impl Hasher for NoneHasher {
+    #[allow(unused_variables)]
+    fn update(&mut self, bytes: &[u8]) {}
+    fn finish(&mut self) -> Vec<u8> {
+        const DRCOM_DIAL_EXT_PROTO_CRC_INIT: u32 = 20000711;
+        let mut result = vec![0u8; 8];
+        NativeEndian::write_u32(result.as_mut_slice(), DRCOM_DIAL_EXT_PROTO_CRC_INIT);
+        NativeEndian::write_u32(&mut result.as_mut_slice()[4..], 126);
+        result
+    }
+}
+
 impl CRCHasher for CRCHasherType {
     fn from_mode(mode: u8) -> Self {
         match mode {
+            0 => CRCHasherType::NONE,
             3 => CRCHasherType::SHA1,
 
             _ => CRCHasherType::MD5,
@@ -44,16 +57,15 @@ impl CRCHasher for CRCHasherType {
 
     fn hasher(&self) -> Box<Hasher> {
         match *self {
+            CRCHasherType::NONE => Box::new(NoneHasher {}) as Box<Hasher>,
             CRCHasherType::SHA1 => HasherBuilder::build(HasherTypes::SHA1),
-            // CRCHasherType::MD5 => HasherBuilder::MD5::build(),
-            //
-            // default md5 for now
             _ => HasherBuilder::build(HasherTypes::MD5),
         }
     }
 
     fn retain_postions(&self) -> Vec<usize> {
         match *self {
+            CRCHasherType::NONE => vec![0, 1, 2, 3, 4, 5, 6, 7],
             CRCHasherType::SHA1 => vec![2, 3, 9, 10, 5, 6, 15, 16],
 
             _ => vec![2, 3, 8, 9, 5, 6, 13, 14],
@@ -85,8 +97,10 @@ fn calculate_drcom_crc32(bytes: &[u8], initial: Option<u32>) -> Result<u32, &'st
 fn test_generate_crc_hash() {
     let crc_hash_md5 = generate_crc_hash(b"1234567890", 1);
     let crc_hash_sha1 = generate_crc_hash(b"1234567890", 3);
+    let crc_hash_none = generate_crc_hash(b"1234567890", 0);
     assert_eq!(crc_hash_md5, vec![241, 252, 155, 176, 45, 19, 56, 161]);
     assert_eq!(crc_hash_sha1, vec![7, 172, 175, 195, 79, 84, 246, 202]);
+    assert_eq!(crc_hash_none, vec![199, 47, 49, 1, 126, 0, 0, 0]);
 }
 
 #[test]
